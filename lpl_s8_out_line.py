48fc9063eed3eb9dc977928847490309bd643bfb
陈以强 <837364695@qq.com>
2018-08-22 14:50:19 +0800

    Update lpl_s8_out_line.py

--- lpl_s8_out_line.py
+++ lpl_s8_out_line.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 from itertools import combinations, permutations
 
-spring_points_dict = {"ig": 30, "rng": 90, "rw": 50, "jdg": 0, "edg": 70}
+spring_points_dict = {"ig": 30, "rng": 90, "rw": 50, "jdg": 0, "edg": 70, "*": 0}
 summer_points_list = [90, 70, 50, 30, 10]
 
 
@@ -9,10 +9,12 @@ def calculator(team_list):
     team_probability = {}
     result_list = []
     # 列出所有可能的排名组合
-    all_combination = list(permutations(team_list, 5))
+    all_combination = list(permutations(team_list, 6))
     for combination in all_combination:
-        if out_line(combination):
-            result_list.append(out_line(combination))
+        # 目前仅可以知晓ig rw不会低于前四
+        result_one = out_line(combination, {"ig": 4, "rw": 4})
+        if result_one:
+            result_list.append(result_one)
     print len(result_list)
     result_tuple = set(result_list)
     for team in result_tuple:
@@ -28,12 +30,13 @@ def calculator(team_list):
         print "{0}:{1}".format(i, percent)
 
 
-def out_line(team_rank_tuple):
+def out_line(team_rank_tuple, team_summer_bottom):
     # 出线计算器
     # 夏季赛排名状况
     team_rank_list = list(team_rank_tuple)
-    if (team_rank_list.index("ig")) == 4 or (team_rank_list.index("rw") == 4):
-        return None
+    for team in team_summer_bottom.keys():
+        if team_rank_list.index(team) >= team_summer_bottom[team]:
+            return False
     team_points_dict = {}
     # 第一名直接出线
     first = team_rank_list[0]
@@ -51,5 +54,5 @@ def out_line(team_rank_tuple):
 
 
 if __name__ == '__main__':
-    team_list = ["ig", "rng", "rw", "jdg", "edg"]
+    team_list = ["ig", "rng", "rw", "jdg", "edg", "*"]
     calculator(team_list)
